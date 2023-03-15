d86bbe9a158664a819fb4b4541b8e9b24980a966
hahayz77 <danilo__danilo@hotmail.com>
2023-03-14 21:13:33 -0300

    sequence worked

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,33 @@ function App() {
         {name: "CCSkin" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/7/7e/T_Amaterasu_CCSkin_Icon.png"}
     ];
 
-    const [commands, setCommands] = useState("commands player");
-    const [teamA, setTeamA] = useState([
-        {name: "Battleworn1" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/5/56/T_Achilles_Battleworn_Icon.png"},
-        {name: "Battleworn2" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/5/56/T_Achilles_Battleworn_Icon.png"},
-        {name: "Battleworn3" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/5/56/T_Achilles_Battleworn_Icon.png"}
-    ]);
-    const [teamB, setTeamB] = useState([
-        {name: "Battleworn3" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/5/56/T_Achilles_Battleworn_Icon.png"},
-        {name: "Battleworn4" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/5/56/T_Achilles_Battleworn_Icon.png"},
-        {name: "Battleworn5" , image: "https://static.wikia.nocookie.net/smite_gamepedia/images/5/56/T_Achilles_Battleworn_Icon.png"}
-    ]);
-    
-    // useEffect(()=>{
-    //     alert(commands)
-    // },[])
-
-    async function Scripts(){
-       
+    const [commands, setCommands] = useState(['command1', 'command2', 'command3','command4','command5',]);
+    const [commandIndex, setCommandIndex] = useState(0);
+    const [teams, setTeams] = useState([{},{},{},{}]);
+    let teamA = teams.slice(0,2);
+    let teamB = teams.slice(2,4);
+        
+    function Scripts(){
+        if(commandIndex <= 1){ 
+            setTeams(() => {teams[commandIndex] = repeat[commandIndex]; return teams});
+            setCommandIndex(result => result = commandIndex + 1)
+        } 
+        if(commandIndex > 1 && commandIndex <= 3){
+            setTeams(() => {teams[commandIndex] = repeat[commandIndex]; return teams});
+            setCommandIndex(result => result = commandIndex + 1)
+        }
     }
 
+    function ImageSet(obj){
+
+    }
+
+    useEffect(()=>{
+        if(commandIndex !== 0){
+            Scripts(commands);
+        }
+    },[commands])
+        
   return (
     <>
         <div className="App bg-danger">
@@ -42,20 +49,19 @@ function App() {
                     <h1>Smite Picks</h1>
                 </div>
                 <div id='playercommands' className="fluid-container bg-success">
-                    {commands}
-                    <button className='btn btn-dark' onClick={Scripts}>Start Script</button>
-                    <button className='btn btn-primary' onClick={Scripts}>Select</button>
+                    {commands[commandIndex]}
+                    <button className='btn btn-primary' onClick={()=>{Scripts(()=>{repeat.find(commands)})}}>Select</button>
                 </div>
                 <div id='maincontent' className="fluid-container">
                     <div className="row">
                         <div id="team1" className='bg-danger col'>
-                           {teamA.map((teamA, index)=>{
+                           {teamA.map((teamA)=>{
                             return(
                                 <div className="row">
                                     <div className="img_container">
-                                        <img src={teamA.image} alt="..."/>
+                                        <img src={teamA.image === undefined ? "" : teamA.image} alt="..."/>
                                     </div>
-                                <span>{teamA.name}</span>
+                                <span>{teamA.name === undefined ? "" : teamA.name}</span>
                                 </div>
                             )
                            })}
@@ -63,7 +69,7 @@ function App() {
                         <div id="gods" className='bg-primary col'>
                            {repeat.map((gods)=>{
                             return(
-                                <div className='gods_images' onClick={()=>{setCommands(gods.name)}}>
+                                <div className='gods_images' onClick={()=>{ImageSet(()=>{repeat.find(repeat.name)})}}>
                                     {gods.name}
                                     <img src={gods.image} alt={gods.name} />
                                 </div>   
@@ -75,7 +81,7 @@ function App() {
                             return(
                                 <div className="row">
                                     <div className="img_container">
-                                        <img src={teamB.image} alt="..."/>
+                                        <img src={teamB.image === undefined ? "" : teamB.image} alt="..."/>
                                     </div>
                                 <span>{teamB.name}</span>
                                 </div>
