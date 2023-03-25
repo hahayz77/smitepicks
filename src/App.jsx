5dcedad0b453f9065a67aa22928e67f3a69ca2ca
hahayz77 <danilo__danilo@hotmail.com>
2023-03-24 22:55:36 -0300

    incial logic

--- src/App.jsx
+++ src/App.jsx
@@ -17,30 +17,53 @@ function App() {
 
     const [commands, setCommands] = useState(['command1', 'command2', 'command3','command4','command5',]);
     const [commandIndex, setCommandIndex] = useState(0);
-    const [teams, setTeams] = useState([{},{},{},{}]);
-    let teamA = teams.slice(0,2);
-    let teamB = teams.slice(2,4);
+    const [imgGod, setImgGod] = useState();
+    const [teamA, setTeamA] = useState([{},{}]);
+    const [teamB, setTeamB] = useState([{},{}]);
+    const [bansA, setBansA] = useState([{},{}]);
+    const [bansB, setBansB] = useState([{},{}]);
+    let pass = true;
+    
+    
+    function Verification(godSelected, commandIndex){
+        // console.log(bansA.find(e => e == godSelected))
+        // if(undefined){
+        //     pass = false;
+        // }
+        // if(bansA.find(e => e == godSelected)){
+        //     console.log("BANISHED") 
+        // }
+
+    }
+    
+    function ImageGod(imgGod){
+        setImgGod(imgGod);
+        if(commandIndex === 0) setBansA([imgGod, {}])
+        if(commandIndex === 1) setBansB([imgGod, {}])
+        if(commandIndex === 2) setTeamA([imgGod, {}])
+        if(commandIndex === 3) setTeamB([imgGod, {}])
+        if(commandIndex === 4) setBansA([bansA[0], imgGod])
+        if(commandIndex === 5) setBansB([bansB[0], imgGod])
+
+        // setTeamA([teamA[0], imgGod]);
+    }
+
+    function MainScript(){
+        if(!imgGod){
+            alert("You should select a God!");
+            return;
+        }
         
-    function Scripts(){
-        if(commandIndex <= 1){ 
-            setTeams(() => {teams[commandIndex] = gods[commandIndex]; return teams});
-            setCommandIndex(result => result = commandIndex + 1)
-        } 
-        if(commandIndex > 1 && commandIndex <= 3){
-            setTeams(() => {teams[commandIndex] = gods[commandIndex]; return teams});
-            setCommandIndex(result => result = commandIndex + 1)
-        }
-    }
-
-    function ImageSet(obj){
+        ImageGod(imgGod);
+        Verification(imgGod, commandIndex);
+        if(!pass) return;
+        
+        setCommandIndex(commandIndex+1);
+        setImgGod();
 
     }
 
-    useEffect(()=>{
-        if(commandIndex !== 0){
-            Scripts(commands);
-        }
-    },[commands])
+   
         
   return (
     <>
@@ -50,7 +73,7 @@ function App() {
                 </div>
                 <div id='playercommands' className="fluid-container bg-success">
                     {commands[commandIndex]}
-                    <button className='btn btn-primary' onClick={()=>{Scripts(()=>{gods.find(commands)})}}>Select</button>
+                    <button className='btn btn-primary' onClick={()=>{MainScript()}}>Select</button>
                 </div>
                 <div id='maincontent' className="fluid-container">
                     <div className="row">
@@ -65,13 +88,25 @@ function App() {
                                 </div>
                             )
                            })}
+                           <div id="bans_team1" className='row'>
+                                {bansA.map((bansA)=>{
+                                    return(
+                                            <div className="col">
+                                                <h1>Bans</h1>
+                                                <div className="img_container my-2">
+                                                    <img src={bansA.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : bansA.image} alt="..."/>
+                                                </div>
+                                            </div>
+                                    )
+                                })} 
+                            </div>
                         </div>
                         <div id="gods" className='bg-dark col'>
-                           {gods.map((gods)=>{
+                           {gods.map((element)=>{
                             return(
-                                <div className='gods_images' onClick={()=>{ImageSet(()=>{gods.find(gods.name)})}}>
-                                    {gods.name}
-                                    <img src={gods.image} alt={gods.name} />
+                                <div className='gods_images' onClick={()=>{ImageGod(element)}}>
+                                    {element.name}
+                                    <img src={element.image} alt={element.name} />
                                 </div>   
                             )
                            })}
@@ -87,6 +122,18 @@ function App() {
                                 </div>
                             )
                            })}
+                            <div id="bans_team1" className='row'>
+                                {bansB.map((bansB)=>{
+                                    return(
+                                            <div className="col">
+                                                <h1>Bans</h1>
+                                                <div className="img_container my-2">
+                                                    <img src={bansB.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : bansB.image} alt="..."/>
+                                                </div>
+                                            </div>
+                                    )
+                                })} 
+                            </div>
                         </div>
                     </div>
                     
