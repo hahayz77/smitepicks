ff3734229fc9e2de99936145bea9fd493fb4d1df
hahayz77 <danilo__danilo@hotmail.com>
2023-04-04 09:10:18 -0300

    new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
 import './App.css';
+import { TeamA } from './components/Team';
 
 function App() {
     const fetchData = [
@@ -31,31 +32,34 @@ function App() {
     const [teamB, setTeamB] = useState([{},{}]);
     const [bansA, setBansA] = useState([{},{}]);
     const [bansB, setBansB] = useState([{},{}]);
-    let pass = true;
-    
-    
-    function Verification(godSelected, commandIndex, testBans, allBans, testPicksA, testPicksB){
-        testBans = undefined;
-        allBans = [...bansA, ...bansB];
-        testPicksA = teamA;
-        testPicksB = teamB;
-        testBans = allBans.find(e => e.name === godSelected.name);
-        if(commandIndex === 6) testPicksA.find(e => e.name === godSelected.name);
-        if(commandIndex === 7) testPicksB.find(e => e.name === godSelected.name);
-        console.log(godSelected, commandIndex, allBans, testPicksA, testPicksB, bansA, bansB, allBans);
-        // if(testBans) pass = false;
-        
-        // if(!test){
-        //     pass = false;
-        // }
-        // console.log(godSelected);
-        // console.log(bansA.find(e => e.name == godSelected.name));
-        // console.log(bansB.find(e => e.name == godSelected.name));
-        // test = bansA.find(e => e.name == godSelected.name);
-        // if(bansA.find(e => e === godSelected)){
-        //     console.log("BANISHED") 
-        // }
+    const [allBans, setAllBans] = useState();
+    const [pass, setPass] = useState(true);
 
+    function MainScript(){
+        if(!god){
+            alert("You should select a God!");
+            return;
+        }
+        
+        PickImageGod(god);  // updates the useState obj
+        Verification(god, commandIndex); // test the Bans and the Picks from the sequence
+        if(pass === false) {
+            alert("Already Banished or picked!");
+            setPass(true); // Reset pass variable for next tests
+            return;
+        }
+        
+        setCommandIndex(commandIndex+1);    // increase the variable to set the next one for the sequence
+        setGod();   // Updates the useState obj
+    }
+    
+    function Verification(godSelected, commandIndex, testBans){
+        testBans = undefined;
+        testBans = allBans?.find(e => e.name === godSelected.name);
+        // if(commandIndex === 6) testPicksA.find(e => e.name === godSelected.name);
+        // if(commandIndex === 7) testPicksB.find(e => e.name === godSelected.name);
+        if(testBans) setPass(false);
+        setAllBans([...bansA, ...bansB]);
     }
     
     function PickImageGod(god){
@@ -69,97 +73,31 @@ function App() {
 
         // setTeamA([teamA[0], god]);
     }
-
-    function MainScript(){
-        if(!god){
-            alert("You should select a God!");
-            return;
-        }
-        
-        PickImageGod(god);  // updates the useState obj
-        Verification(god, commandIndex); // test the Bans and the Picks from the sequence
-        if(!pass) {
-            alert("Already Banished or picked!")
-            return;
-        }
-        
-        setCommandIndex(commandIndex+1);    // increse the variable to set the next one for the sequence
-        setGod();   // Updates the useState obj
-
-    }
-
    
         
   return (
     <>
         <div className="App">
-                <div id='title' className="container">
+                <div id='title'>
                     <h1 className='title_section'>Smite Picks</h1>
-                </div>
-                <div id='playercommands' className="fluid-container bg-success">
-                    {commands[commandIndex]}
-                    <button className='btn btn-primary' onClick={()=>{MainScript()}}>Select</button>
+                    <div id='playercommands'>
+                        {commands[commandIndex]}
+                        <button className='btn btn-primary' onClick={()=>{MainScript()}}>Select</button>
+                    </div>
                 </div>
                 <div id='maincontent' className="fluid-container">
-                        <div id="team1" className=''>
-                           {teamA.map((teamA, index)=>{
-                            return(
-                                <div className="row player_container">
-                                    <div className="img_container my-2">
-                                        <img src={teamA.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : teamA.image} alt="..."/>
-                                    </div>
-                                    <span>{teamA.name === undefined ? "Player"+(index+1) : teamA.name}</span>
-                                </div>
-                            )
-                           })}
-                           <div id="bans_team1" className='row'>
-                                {bansA.map((bansA)=>{
-                                    return(
-                                            <div className="col">
-                                                <h1>Bans</h1>
-                                                <div className="img_container my-2">
-                                                    <img src={bansA.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : bansA.image} alt="..."/>
-                                                </div>
-                                            </div>
-                                    )
-                                })} 
-                            </div>
-                        </div>
+                        <TeamA team={teamA} bans={bansA} name={"teamA"}/>
                         <div id="gods" className='bg-dark col'>
                            {fetchData.map((god)=>{
                             return(
                                 <div className='gods_images' onClick={()=>{PickImageGod(god)}}>
-                                    {god.name}
+                                    <span>{god.name}</span>
                                     <img src={god.image} alt={god.name} />
                                 </div>   
                             )
                            })}
                         </div>
-                        <div id="team2" className='bg-danger col'>
-                        {teamB.map((teamB, index)=>{
-                            return(
-                                <div className="row">
-                                    <div className="img_container my-2">
-                                        <img src={teamB.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : teamB.image} alt="..."/>
-                                    </div>
-                                    <span>{teamB.name === undefined ? "Player"+(index+3) : teamB.name}</span>
-                                </div>
-                            )
-                           })}
-                            <div id="bans_team1" className='row'>
-                                {bansB.map((bansB)=>{
-                                    return(
-                                            <div className="col">
-                                                <h1>Bans</h1>
-                                                <div className="img_container my-2">
-                                                    <img src={bansB.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : bansB.image} alt="..."/>
-                                                </div>
-                                            </div>
-                                    )
-                                })} 
-                            </div>
-                        </div>
-                    
+                        <TeamA team={teamB} bans={bansB} name={"teamB"}/>
                 </div>
         </div>
 
---
+++ src/components/Team.jsx
@@ -0,0 +1,30 @@
+export function TeamA({team, bans, name}) {
+    return (
+        <>
+            <div id={name}>
+                {team.map((team, index)=>{
+                return(
+                    <div key={`${name}${index}`} className="player_container">
+                        <div className="img_container my-2">
+                            <img src={team.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : team.image} alt="..."/>
+                        </div>
+                        <span>{team.name === undefined ? "Player"+(index+1) : team.name}</span>
+                    </div>
+                )
+                })}
+                <div id={`bans_${name}`}>
+                    <h1 className="container_title">Bans</h1>
+                    <div className="bans_container">
+                    {bans.map((bans, index)=>{
+                        return(
+                                <div key={`${name}${index}`} className="img_container">
+                                    <img src={bans.image === undefined ? "https://cdn.shopify.com/s/files/1/0735/7415/products/smite-pentakill_2000x.jpg?v=1542612477" : bans.image} alt="..."/>
+                                </div>
+                        )
+                    })} 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
