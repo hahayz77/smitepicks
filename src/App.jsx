1e6960d6549740b5c3702de9033d35620ebc611e
hahayz77 <danilo__danilo@hotmail.com>
2023-04-07 09:27:26 -0300

    modular app main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { TeamA } from './components/Team';
+import { Verification } from './functions/Verification';
+import { PickImageGod } from './functions/PickImageGod';
 
 function App() {
     const fetchData = [
@@ -17,14 +19,15 @@ function App() {
     ];
 
     const commands = [
-        '1 Bans TA',
-        '1 Bans TB',
-        '1 PICK TA',
-        '1 PICK TB',
-        '2 BAN TA',
-        '2 BAN TB',
-        '2 PICK TA',
-        '2 PICK TB',
+        'Select to start the picks!',
+        '1º BAN',
+        '1º BAN',
+        '1º PICK',
+        '1º PICK',
+        '2º BAN',
+        '2º BAN',
+        '2º PICK',
+        '2º PICK',
     ];
     const [commandIndex, setCommandIndex] = useState(0);
     const [god, setGod] = useState();
@@ -34,15 +37,27 @@ function App() {
     const [bansB, setBansB] = useState([{},{}]);
     const [allBans, setAllBans] = useState();
     const [pass, setPass] = useState(true);
+    const [playerSelector, setPlayerSelector] = useState();
+
+    useEffect(()=>{
+        if(commandIndex === 0) setPlayerSelector("⚫");
+        else if((commandIndex % 2) === 0) setPlayerSelector("🔴");
+        else setPlayerSelector("🔵");
+    },[commandIndex])
 
     function MainScript(){
-        if(!god){
+        if(commandIndex === 0) { // click select to start picking
+            setCommandIndex(commandIndex+1);
+            return;
+        }
+        if(!god) {
             alert("You should select a God!");
             return;
         }
         
-        PickImageGod(god);  // updates the useState obj
-        Verification(god, commandIndex); // test the Bans and the Picks from the sequence
+        PickImageGod(god, setGod, commandIndex, bansA, setBansA, bansB, setBansB, setTeamA, setTeamB);  // updates the useState obj
+        Verification(god, commandIndex, setPass, allBans, setAllBans, bansA, bansB); // test the Bans and the Picks from the sequence
+        
         if(pass === false) {
             alert("Already Banished or picked!");
             setPass(true); // Reset pass variable for next tests
@@ -52,27 +67,6 @@ function App() {
         setCommandIndex(commandIndex+1);    // increase the variable to set the next one for the sequence
         setGod();   // Updates the useState obj
     }
-    
-    function Verification(godSelected, commandIndex, testBans){
-        testBans = undefined;
-        testBans = allBans?.find(e => e.name === godSelected.name);
-        // if(commandIndex === 6) testPicksA.find(e => e.name === godSelected.name);
-        // if(commandIndex === 7) testPicksB.find(e => e.name === godSelected.name);
-        if(testBans) setPass(false);
-        setAllBans([...bansA, ...bansB]);
-    }
-    
-    function PickImageGod(god){
-        setGod(god);
-        if(commandIndex === 0) setBansA([god, {}])
-        if(commandIndex === 1) setBansB([god, {}])
-        if(commandIndex === 2) setTeamA([god, {}])
-        if(commandIndex === 3) setTeamB([god, {}])
-        if(commandIndex === 4) setBansA([bansA[0], god])
-        if(commandIndex === 5) setBansB([bansB[0], god])
-
-        // setTeamA([teamA[0], god]);
-    }
    
         
   return (
@@ -81,8 +75,9 @@ function App() {
                 <div id='title'>
                     <h1 className='title_section'>Smite Picks</h1>
                     <div id='playercommands'>
-                        {commands[commandIndex]}
                         <button className='btn btn-primary' onClick={()=>{MainScript()}}>Select</button>
+                        <span>  {commands[commandIndex]}</span>
+                        <span> {playerSelector} </span>
                     </div>
                 </div>
                 <div id='maincontent' className="fluid-container">
@@ -90,7 +85,7 @@ function App() {
                         <div id="gods" className='bg-dark col'>
                            {fetchData.map((god)=>{
                             return(
-                                <div className='gods_images' onClick={()=>{PickImageGod(god)}}>
+                                <div className='gods_wrapper' onClick={()=>{PickImageGod(god, setGod, commandIndex, bansA, setBansA, bansB, setBansB, setTeamA, setTeamB)}}>
                                     <span>{god.name}</span>
                                     <img src={god.image} alt={god.name} />
                                 </div>   
